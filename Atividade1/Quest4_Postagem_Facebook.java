ae7b9d30f4c737b79031b51c07d5ff9338eafa67
MatiasMycaelly <89050717+MatiasMycaelly@users.noreply.github.com>
2021-10-15 16:59:43 -0300

    Create Quest4_Postagem_Facebook.java

---
+++ Atividade1/Quest4_Postagem_Facebook.java
@@ -0,0 +1,21 @@
+public class Quest4_Postagem_Facebook {
+
+    public static void main(String[] args) {
+
+        Quest4_Postagem p1 = new Quest4_Postagem(
+                "Se a terra é plana, por que dizemos aquecimento goblal ao inves de a chapa está esquentando?",
+                "https://www.instagram.com");
+
+        p1.curtiPost();
+        p1.curtiPost();
+        p1.curtiPost();
+
+        System.out.println("Quantidade de curtidas: " + p1.numeroCurtidas);
+
+        p1.compartilharPost();
+        p1.compartilharPost();
+
+        System.out.println("Quantidade de compartilhamentos: " + p1.numeroCompartilhamento);
+
+    }
+}

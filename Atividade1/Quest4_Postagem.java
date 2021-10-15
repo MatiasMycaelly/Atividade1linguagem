6fb471cd92815e6c66fe52a916131e45c0f5d6bf
MatiasMycaelly <89050717+MatiasMycaelly@users.noreply.github.com>
2021-10-15 16:59:38 -0300

    Create Quest4_Postagem.java

---
+++ Atividade1/Quest4_Postagem.java
@@ -0,0 +1,21 @@
+public class Quest4_Postagem {
+
+    String text, link;
+    int numeroCurtidas, numeroCompartilhamento;
+
+    public Quest4_Postagem(String text, String link) {
+
+        this.text = text;
+        this.link = link;
+
+    }
+
+    public void curtiPost() {
+        this.numeroCurtidas++;
+    }
+
+    public void compartilharPost() {
+        this.numeroCompartilhamento++;
+    }
+
+}
